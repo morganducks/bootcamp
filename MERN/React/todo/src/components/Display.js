582eee0a6d33e4170c0e87e5eecb047d97ffa1e6
Paul Morgan <morganducks@gmail.com>
2022-02-25 22:18:11 -0800

    enough

--- MERN/React/todo/src/components/Display.js
+++ MERN/React/todo/src/components/Display.js
@@ -10,10 +10,11 @@ const {todoList, setTodoList} = props;
 const deleteButton = (id) => {
     //filters todoList, which is populated by getTodoList
     //returns new array of every todo whose id does NOT match the id from below, leaving only the element with the id assigned to the function
-    //filters out all id tags except the id from below. If you declare "== idFromBelow" then every OTHER element is deleted because the filter sets the value ONLY the id assigned to the function
+    //If you declare "== idFromBelow" then every OTHER element is deleted because the filter sets the value ONLY the id assigned to the function
     setTodoList(todoList.filter((todo, index) => todo.id !== id))
 }
 
+//sets class from 
 const styled = (markedChecked) => {
     //conditional
     if(markedChecked === true) {
@@ -40,7 +41,7 @@ const styled2 = (markedChecked) => {
 
 
 //I'm not sure how this works still
-
+//when function called it evaluates todo?
     function handleChecked(todo) {
         //parses whether todo is markedChecked
         todo.markChecked = !todo.markChecked;
@@ -54,15 +55,17 @@ const styled2 = (markedChecked) => {
 return(
     <div>
         {
-            //tracks every "todo" added to the 
+            //identifies array elements and places them in "todo"
        todoList.map((todo, index) => (
            <div key={index}>
                {/* class as a funtion "styled", changes CSS tag based on boolean */}
                <p className={styled(todo.markChecked)}>{todo.todoText}</p>
-               {/* boolean function whether the box is checked T/F, assigned to todo */}
+               {/* runs boolean function whether the box is checked T/F, assigned to todo 
+               what is a callback? Callback to "todo?" */}
                <input onClick={() => handleChecked(todo) }
                    type="checkbox" />
-                   {/*when clicked passes the .id for todo to const above*/}
+                   {/*when clicked passes the .id for todo to const above and runs "deleteButton" 
+                                   brackets introduce funtion? */}
                <button className={styled2(todo.markChecked)} onClick={()=> deleteButton(todo.id) }>
                    Delete</button>
                    </div>
