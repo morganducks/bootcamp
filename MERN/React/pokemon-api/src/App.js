b42f7b187afac6961fe909fe35d1d6a56e538056
Paul Morgan <morganducks@gmail.com>
2022-03-03 19:52:24 -0800

    axios api

--- MERN/React/pokemon-api/src/App.js
+++ MERN/React/pokemon-api/src/App.js
@@ -1,49 +1,35 @@
 import './App.css';
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-//              getter  setter       establishes useState as empty array
-// anytime array changes useState changes
-    const [pokemonNames, setPokemonNames] = useState([]); 
-    
-    //useEffect runs through 3 states on render or change: did mount, did update, will unmount
-    useEffect(() => {
 
-      //when I change the limit upwards I see a lot more pokemon 
-      //when I set no limit I get about 20 results. Why? 
-      fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-        .then(response => {
-          //tells program to pull the json from API link above
-          return response.json()
-        })
-        .then(response => { 
-          //puts the results in pokemonNames
-          setPokemonNames(response.results)
-        })
+  //        getter        setter
+  const [pokemonNames, setPokemonNames] = useState([]);
 
-      .catch((err) => {
-            console.log(err);
-          })
-    }, []);
-    
 
-        return(
-        <div className="App">
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(response => {
+        //sets results from API into pokemonNames array
+        setPokemonNames(response.data.results);
+      })
+      .catch((err => console.log(err)))
+  }, []);
+
+
+  return (
+    <div className="App">
       <ul>
-        {
-          //getter then map places those elements in new object pokemonNamesObj
-          pokemonNames.map((pokemonNamesObj, index)=>{
-            //looks for the .name value and displays in a list
-            return (<li key={index}>{pokemonNamesObj.name}</li>)
-          })
-        }
+        {/* identifies data and creates new array, places in pokemon variable */}
+        {pokemonNames.map((pokemon, index) => (
+          // runs tells li to reference array and populate pokemon.value
+          <li key={index}><a href={pokemon.url}>{pokemon.name}</a></li>
+        ))}
       </ul>
     </div>
-    
-        );
-
-
-    
+  )
 }
 
 export default App;
